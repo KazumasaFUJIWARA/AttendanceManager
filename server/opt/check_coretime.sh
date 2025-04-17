d74d36df7c48ebd0acc5efeb3a8c1459aac1151c
KazumasaFUJIWARA <kazumasa.fujiwara@gmail.com>
2025-04-17 13:44:19 +0900

    fix: Dockerコンテナ内からホストのAPIにアクセスできるようにlocalhostをhost.docker.internalに変更

--- server/opt/check_coretime.sh
+++ server/opt/check_coretime.sh
@@ -11,7 +11,7 @@ import datetime
 
 def check_coretime(check_number):
     try:
-        response = requests.post(f'http://localhost:8000/core-time/check/{check_number}')
+        response = requests.post(f'http://host.docker.internal:8889/api/core-time/check/{check_number}')
         print(f'[{datetime.datetime.now()}] Core-time check {check_number}: {response.status_code}')
     except Exception as e:
         print(f'[{datetime.datetime.now()}] Error in core-time check {check_number}: {str(e)}')
